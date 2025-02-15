66eab7ce2e0d3ed13b520fd6fbee5bac219ac338
Eatcoding <trivialcoding6@gmail.com>
2025-02-15 11:31:43 +0900

    배포 완료

--- app/db/base.py
+++ app/db/base.py
@@ -1,15 +1,7 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from app.core.config import Settings
-
-settings = Settings()
-
-# 동기 엔진
-engine = create_engine(
-    settings.DATABASE_URL,
-    echo=True,
-)
+from app.core.config import settings
 
 # 비동기 엔진
 async_engine = create_async_engine(
@@ -18,9 +10,14 @@ async_engine = create_async_engine(
 )
 
 async_session = sessionmaker(
-    async_engine, class_=AsyncSession, expire_on_commit=False
+    async_engine, 
+    class_=AsyncSession, 
+    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False
 )
 
 # 데이터베이스 테이블 생성
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+async def create_db_and_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
