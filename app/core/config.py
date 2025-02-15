526500ac75a572766b0df14634c6e5ad5c453b21
Eatcoding <trivialcoding6@gmail.com>
2025-02-15 12:20:24 +0900

    README 추가

--- app/core/config.py
+++ app/core/config.py
@@ -11,7 +11,6 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: str
     
-    # DATABASE_URL을 SQLALCHEMY_DATABASE_URI로 매핑하는 프로퍼티 추가
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> str:
         return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
