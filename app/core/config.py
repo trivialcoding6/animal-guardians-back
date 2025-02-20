9441d30b76ca2a33ffc55c9401943f45a7151250
Eatcoding <trivialcoding6@gmail.com>
2025-02-20 14:43:08 +0900

    config 이름 겹치는 거 수정

--- app/core/config.py
+++ app/core/config.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
     CONTAINER_NAME: str
     
     @property
-    def DATABASE_URL(self) -> str:
+    def ASYNC_DATABASE_URL(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
     @property
