537a0134bf8f47c3b313a6c09fdb067aa9f4a8c9
Zoonhyeong <wkcpq35@naver.com>
2025-02-21 01:50:04 +0900

    BaseModel에 filename 필드 수정

--- app/schemas/upload.py
+++ app/schemas/upload.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel
 
 class Upload(BaseModel):
-    filename: str  # 이전에는 UUID 기반 파일명
-    filename: str  # 원본 파일명
+    filename: str
     url: str
     class Config:
         schema_extra = {
