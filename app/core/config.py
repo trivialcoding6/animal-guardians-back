05b247dd3553858d31d282bd9166d84b63d7e752
Zoonhyeong <wkcpq35@naver.com>
2025-02-21 10:29:16 +0900

    config 파일 수정

--- app/core/config.py
+++ app/core/config.py
@@ -16,14 +16,12 @@ class Settings(BaseSettings):
     DATABASE_URL: str
 
     AZURE_CONNECTION_STRING: str
-    CONTAINER_NAME: str
-    
+    STORAGE_NAME: str
+
     PREDICTION_ENDPOINT: str
     PREDICTION_KEY: str
     PROJECT_ID: str
     MODEL_NAME: str
-    
-    STORAGE_NAME: str
     
     @property
     def ASYNC_DATABASE_URL(self) -> str:
